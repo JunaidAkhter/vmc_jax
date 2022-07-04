5c96187901ae9f68096b92395971e4bff1399859
Junaid Akhter <akhter@iff1544.iff.kfa-juelich.de>
2022-07-04 16:21:02 +0200

    updating the initializer

--- jVMC/nets/initializers.py
+++ jVMC/nets/initializers.py
@@ -1,16 +1,50 @@
+from typing import NewType
 import jax
 from jax.config import config
 config.update("jax_enable_x64", True)
 import flax
 from flax import nn
-
+import jax.numpy as jnp
 import jVMC.global_defs as global_defs
+from functools import partial 
 
+def rl_init(rng, shape):
+
+    #unif = jax.nn.initializers.uniform()
+    
+    #unif = jax.nn.initializers.uniform(scale = 0.1)
+    unif = jax.nn.initializers.normal(stddev = 0.155)
+    return unif(rng, shape, dtype=global_defs.tReal)
 
 def cplx_init(rng, shape):
     rng1, rng2 = jax.random.split(rng)
-    unif = jax.nn.initializers.uniform()
+    #unif = jax.nn.initializers.uniform()
+    unif = jax.nn.initializers.uniform(scale = 0.01)
+    #unif = jax.nn.initializers.normal(stddev = 0.01)
     return unif(rng1, shape, dtype=global_defs.tReal) + 1.j * unif(rng2, shape, dtype=global_defs.tReal)
+
+def rl_init1(rng, shape, var):
+
+    """This function iniliatizes the real parameters with given variance, "var" """
+    
+    #unif = jax.nn.initializers.uniform(scale = 0.1)
+    norm = jax.nn.initializers.normal(stddev = jnp.sqrt(var))
+    return norm(rng, shape, dtype=global_defs.tReal)
+
+
+def cplx_init1(rng, shape, var):
+    """This function initializes the real and the complex part of the complex initialization
+    by using the fact that Var[Z] = Var[Real(Z)] + Var[Img(Z)]"""
+    
+    rng1, rng2 = jax.random.split(rng)
+    sigma_1_r = jnp.sqrt(var/2)
+    sigma_1_i = sigma_1_r
+    norm1 = jax.nn.initializers.normal(stddev = sigma_1_r)
+    norm2 = jax.nn.initializers.normal(stddev = sigma_1_i) 
+
+    return norm1(rng1, shape, dtype=global_defs.tReal) + 1.j * norm2(rng2, shape, dtype=global_defs.tReal)
+
+
 
 
 def cplx_variance_scaling(rng, shape):
